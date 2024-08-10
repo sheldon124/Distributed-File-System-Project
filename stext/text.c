eae024f5ae3eb9aac9331d667fe67333ebb150a7
Sheldon Kevin Correa <correas@delta.cs.uwindsor.ca>
2024-08-10 18:56:12 -0400

    added ufile functionality for smain, spdf & stext

--- stext/text.c
+++ stext/text.c
@@ -1,10 +1,107 @@
-#include <netinet/in.h> //structure for storing address information
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h> //for socket APIs
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h> 
-#include <time.h> 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int ufilecommand(char *cmd, char *filename, char *dest, int client) {
+    int pathprovided = 1;
+    char destpath[100];
+    struct stat st;
+
+    if (strcmp(dest, "/") == 0) {
+        pathprovided = 0;
+        strcpy(destpath, "");
+    }
+    else
+        strcpy(destpath, dest);
+
+    if (pathprovided && (stat(destpath, &st) != 0 || !S_ISDIR(st.st_mode))) {
+        // Directory does not exist, attempt to create it
+        char newpath[100] = "";
+        int count = 0;
+        char temppath[100];
+        strcpy(temppath, destpath);
+        for (char *separator = strtok(temppath, "/"); separator != NULL; separator = strtok(NULL, "/"), count += 1) {
+            if (count != 0)
+                strcat(newpath, "/");
+            strcat(newpath, separator);
+            if (stat(newpath, &st) != 0 || !S_ISDIR(st.st_mode)) {
+                if (mkdir(newpath, 0700) != 0) {
+                    perror("mkdir failed");
+                    return -1; // Failed to create directory
+                }
+            }
+        }
+    }
+
+    int filesize;
+    char filesizebuf[100];
+    // get size of file
+    int recbytes = read(client, filesizebuf, 100);
+    if (recbytes < 0)
+    {
+        printf("\nError receiving filesize\n");
+        return 1;
+    }
+
+    filesize = atoi(filesizebuf);
+
+    if (pathprovided)
+    {
+        strcat(destpath, "/");
+        strcat(destpath, filename);
+    }
+    else
+        strcpy(destpath, filename);
+
+    int fd = open(destpath, O_CREAT | O_RDWR | O_APPEND, 0777);
+    if (fd < -1)
+    {
+        printf("\nError opening file\n");
+        return 1;
+    }
+
+    char *message = "received";
+    send(client, message, strlen(message), 0);
+
+    int totalbytesread = 0;
+    char filebuf[100];
+
+    while (totalbytesread < filesize)
+    {
+        // int bytesread = read(client, filebuf, 100);
+        int bytesread = recv(client, filebuf, 100, 0);
+        totalbytesread += bytesread;
+        int writebytes = write(fd, filebuf, bytesread);
+        if (writebytes < 0)
+        {
+            printf("\nError writing file\n");
+            return 1;
+        }
+    }
+
+    printf("\nDone\n");
+
+    return 0;
+}
+
+int prcclient(char* inputcommand, int client) {
+    char cmd[100];
+    char filename[100];
+    char dest[100];
+    sscanf(inputcommand, "%s %s %s", cmd, filename, dest);
+    if(strcmp(cmd, "ufile") == 0) {
+        return ufilecommand(cmd, filename, dest, client);
+    }
+    return 0;
+}
   
 int main(int argc, char *argv[]){//E.g., 1, server
 char *myTime;
@@ -47,13 +144,14 @@ while(1) {
     int pid = fork();
     
     if (pid == 0) {
-    char buff1[50];
-    int bytes_read = read(client, buff1, 50);
+    char buff1[100];
+    int bytes_read = read(client, buff1, 100);
     if (bytes_read < 0) {
         printf("Server: read() failure\n");
         exit(3);
     }
-    printf("%s", buff1);
+    printf("%s\n", buff1);
+    prcclient(buff1, client);
     close(client);
     printf("\n");
     exit(0);
